a3bd0d5a8b14f686f4dc975b415ae61359ea8738
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-02-20 19:59:13 -0500

    setup: update setup

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 setup(
-    name="kamaji",
+    name="scrak",
     version="0.0.0",
     author="Hart Traveller",
-    url="https://github.com/harttraveller/kamaji",
+    url="https://github.com/harttraveller/scrak",
     license="MIT",
     long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["rich", "requests"],
-    entry_points={"console_scripts": ["kamaji=kamaji.cli:cli"]},
+    entry_points={"console_scripts": ["scrak=scrak.cli:entry"]},
 )
